57a13b3aaff75da8a5915a3651751bfef26d2c48
John Hsu <hsu@willowgarage.com>
2009-09-01 18:17:38 +0000

    moving pr2_ogre into simulator_gazebo stack since rviz no longer needs it.  turn back on mesh generation at build-time.

--- pr2_ogre/generate_meshes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PR2_DEFS=`rospack find pr2_defs`
-PR2_OGRE=`rospack find pr2_ogre`
-
-mkdir -p $PR2_OGRE/Media/models/convex
-
-rosrun ogre_tools stl_to_mesh $PR2_DEFS/meshes/*.stl $PR2_OGRE/Media/models
-rosrun ogre_tools stl_to_mesh $PR2_DEFS/meshes/convex/*.stlb $PR2_OGRE/Media/models/convex
